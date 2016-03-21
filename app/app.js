8ec1cafac0026749f5a5e0b9f3a9a64985bc917d
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2016-03-21 13:58:45 -0400

    Rename app from myApp (which is inherited from the angular seed project) to weatherDashboard

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('weatherDashboard', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'weatherDashboard.view1',
+  'weatherDashboard.view2',
+  'weatherDashboard.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
