62ef366bc657885c5999a6272e5a61b5d150ab96
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2016-03-21 22:39:24 -0400

    Add basic time series graph
    Copied from http://jtblin.github.io/angular-chart.js/

--- app/app.js
+++ app/app.js
@@ -3,8 +3,28 @@
 // Declare app level module which depends on views, and components
 angular.module('weatherDashboard', [
   'ngRoute',
-  'weatherDashboard.version'
+  'weatherDashboard.version',
+  'chart.js'
 ])
+.controller("LineCtrl", ['$scope', '$timeout', function ($scope, $timeout) {
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+
+  // Simulate async data update
+  $timeout(function () {
+    $scope.data = [
+      [28, 48, 40, 19, 86, 27, 90],
+      [65, 59, 80, 81, 56, 55, 40]
+    ];
+  }, 3000);
+}])
 .controller('GraphsCtrl', ['$http', function($http) {
   /*
    Codes for heat alert: HA, HAE, EHAD
@@ -65,6 +85,14 @@ angular.module('weatherDashboard', [
     return graph.name == checkGraph;
   }
 }])
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/heatAlert'});
+.config(['ChartJsProvider', function (ChartJsProvider) {
+  // Configure all charts
+  ChartJsProvider.setOptions({
+    colours: ['#FF5252', '#FF8A80'],
+    responsive: false
+  });
+  // Configure all line charts
+  ChartJsProvider.setOptions('Line', {
+    datasetFill: false
+  });
 }]);
